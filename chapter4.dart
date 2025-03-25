fe8b3c2bf83a274f5b5f1d25c4abb7c0d4c1d395
Disha <disha.sahay2005@gmail.com>
2025-03-25 21:35:35 +0530

    CH4 AND CH5

---
+++ chapter4.dart
@@ -0,0 +1,23 @@
+main() {
+  //   print("hello world");
+  //   var map_name={
+  //     'key1': 'value1',
+  //     'key2':2,
+  //     'key3':3.5,
+  //     'key4':true,
+  //   };
+  //   print(map_name);
+  //   print(map_name['key2']);
+  //   print(map_name['key6']);
+
+  // OTHER WAY TO CREATE MAP:-
+
+  var mapName = Map();
+  mapName['Name'] = "Diya";
+  mapName['year of experience'] = "7";
+  mapName['averageRating'] = "4.0";
+  print(mapName);
+  print(mapName.length);
+  print(mapName.isEmpty);
+  print(mapName.isNotEmpty);
+}
---
+++ chapter5.dart
@@ -0,0 +1,10 @@
+main() {
+  print("welcome to dart");
+  // final String name;
+  // name="riya";
+  // print(name);
+  final names = ["riya", "kaya", "siya"];
+  // names=["sitya","tiya","tanya"];
+  // the final keyword is used to declare variables that can be assigned only once but are initialized at runtime (not at compile-time).
+  // IN FINAL WE CAN CHANGE DATA BUT IN CONST WE CANNOT CHANGE IN DATA
+}
